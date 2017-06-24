8c9f063855f10735ae42a0c362a56a50178a3bfa
fukhan <fukhan389@hotmail.com>
2017-06-24 12:01:00 +1000

    test

---
+++ Module1/src/FirstJavaProgram.java
@@ -0,0 +1,25 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Print something");
+int i=5;
+while(i>0)
+{
+	System.out.println(i);
+i--;	
+}
+int sum=0;
+for(i=0;i<5;i++)
+{
+	sum=sum+i;
+	System.out.println(sum);
+	
+}
+System.out.println(sum);
+
+	}
+	
+
+}
